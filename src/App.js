991b89d39d6274c38393a4a3fa22936ab3a8f28a
Ivan <duremar98@yandex.ru>
2021-05-18 18:33:45 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Content, List, Info, Item } from "./style/app_style";
+import { getAxios } from "./components/axios";
 
-function App() {
-  return <div className="App">55</div>;
-}
+const App = () => {
+  const [items, setItems] = useState(null);
+
+  console.log(items);
+
+  useEffect(() => {
+    getAxios().then((response) => {
+      setItems(response);
+    });
+  }, []);
+
+  return (
+    <Content>
+      {items && (
+        <>
+          <List>
+            {items.map((item, index) => {
+              return (
+                <Item key={index}>
+                  <li>{item.id}</li>
+                  <li>{item.firstName}</li>
+                  <li>{item.lastName}</li>
+                  <li>{item.email}</li>
+                  <li>{item.phone}</li>
+                </Item>
+              );
+            })}
+          </List>
+          <Info>44</Info>
+        </>
+      )}
+    </Content>
+  );
+};
 
 export default App;
+
+// console.log(getAxios());
---
+++ src/style/app_style.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  max-width: 1366px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const List = styled.section`
+  flex: 0 0 75%;
+  background-color: #bc8f8f;
+`;
+export const Info = styled.section`
+  flex: 0 0 25%;
+  background-color: #cd853f;
+`;
+
+export const Item = styled.ul``;
