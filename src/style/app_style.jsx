8a9132ad155386683a5070e0f8760fe4fb6bc0c0
Ivan <duremar98@yandex.ru>
2021-05-18 18:58:09 +0300

    app

--- src/style/app_style.jsx
+++ src/style/app_style.jsx
@@ -1,19 +1,84 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  user-select: none;
+}
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+}
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+a {
+  text-decoration: none;
+}
+ul li {
+  list-style: none;
+}
+button {
+  outline: none;
+  cursor: pointer;
+}
+input{
+  outline: none;
+}
+`;
 
 export const Content = styled.div`
   max-width: 1366px;
   height: 100vh;
+  background-color: #8b4513;
+  padding: 8px 5px;
   display: flex;
   flex-direction: column;
 `;
 
 export const List = styled.section`
   flex: 0 0 75%;
-  background-color: #bc8f8f;
+  background-color: #deb887;
+  overflow-y: scroll;
 `;
 export const Info = styled.section`
   flex: 0 0 25%;
-  background-color: #cd853f;
+  background-color: #d2b48c;
 `;
 
-export const Item = styled.ul``;
+export const Item = styled.ul`
+  display: flex;
+  padding: 5px;
+  cursor: pointer;
+  &:not(:last-child) {
+    border-bottom: 1px solid #8b4513;
+  }
+  li {
+    &:nth-child(1) {
+      flex: 0 0 10%;
+    }
+    &:nth-child(2) {
+      flex: 0 0 20%;
+    }
+    &:nth-child(3) {
+      flex: 0 0 20%;
+    }
+    &:nth-child(4) {
+      flex: 0 0 32%;
+    }
+    &:nth-child(5) {
+      flex: 0 0 18%;
+    }
+  }
+  &:hover {
+    background-color: #fff;
+  }
+`;
